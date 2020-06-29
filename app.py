2d19b401277cec95abe926b27d5371191d06ab61
dejvoss <deosiecki@gmail.com>
2020-06-29 20:30:45 +0200

    Add movie to the index.html

--- app.py
+++ app.py
@@ -12,6 +12,10 @@ app = Flask (__name__)
 def home():
     return render_template("index.html")
 
+@app.route('/add_recipe')
+def add_recipe():
+    return render_template("add_recipe.html")
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
