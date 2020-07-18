f04866243656e7cf6139a2246b7a5d06f0aacd6a
dejvoss <deosiecki@gmail.com>
2020-07-18 17:58:54 +0200

    Add insert recipe function and mongoDB configuration variables

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 # Recipe App
 # Flask application for website recipe book
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
 
+if os.path.exists("env.py"):
+    import env
 
-app = Flask (__name__)
+app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'recipe_book'
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
+
+mongo = PyMongo(app)
 
 @app.route('/')
 @app.route('/home')
@@ -16,6 +23,11 @@ def home():
 def add_recipe():
     return render_template("add_recipe.html")
 
+@app.route('/insert_recipe', methods=['POST'])
+def insert_recipe():
+    recipe = mongo.db.recipes
+    recipe.insert_one(request.form.to_dict())
+    return redirect(url_for('home'))
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
