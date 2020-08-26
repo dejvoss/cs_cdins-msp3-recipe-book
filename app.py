8907744f6c712f72a0e04815d0a1fd7f4846c367
dejvoss <deosiecki@gmail.com>
2020-08-26 22:24:21 +0200

    Add python function for saving and uploading images to the database

--- app.py
+++ app.py
@@ -3,6 +3,8 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
+from werkzeug.utils import secure_filename
+import datetime
 
 if os.path.exists("env.py"):
     import env
@@ -11,6 +13,9 @@ app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = 'recipe_book'
 app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
+app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
+app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.gif', '.JPG', '.PNG', '.GIF']
+app.config["IMAGE_UPLOADS"] = "static/images/uploads"
 
 mongo = PyMongo(app)
 
@@ -25,8 +30,16 @@ def add_recipe():
 
 @app.route('/insert_recipe', methods=['POST'])
 def insert_recipe():
-    recipe = mongo.db.recipes
-    recipe.insert_one(request.form.to_dict())
+    todaysDate = datetime.datetime.now()    #today date and time for imange file name
+    string_date = todaysDate.strftime("%Y%m%d %H:%M:%S") # convert date, time to string
+    recipe = mongo.db.recipes # database collection
+    recipe_object = request.form.to_dict() # render the html for to the dictionary object
+    meal_img = request.files['meal_image'] # uploaded image of meal
+    meal_img = secure_filename(meal_img.filename) # change name of image by secure_filename
+    my_img_name = recipe_object["recipe_title"] + '.' + string_date + '.' + meal_img #create own file name to be uniq
+    mongo.save_file(my_img_name, request.files["meal_image"]) # save img meal file to the mongo database
+    recipe_object['meal_image'] = my_img_name #change the name of recipe image to own name created above(my_img_name)
+    recipe.insert_one(recipe_object) #insert recipe_object to the database
     return redirect(url_for('home'))
 
 if __name__ == "__main__":
