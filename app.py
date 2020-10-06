d1597c4c891d7984a8607417707b6b62c7a34769
dejvoss <deosiecki@gmail.com>
2020-10-06 09:11:32 +0200

    Move forms to seperate file and improt these in app.py

--- app.py
+++ app.py
@@ -9,13 +9,14 @@ import pdfkit
 from flask_mail import Mail, Message
 from flask_pymongo import PyMongo
 from werkzeug.utils import secure_filename
-from flask_wtf import FlaskForm
-from wtforms import StringField, DecimalField, SubmitField, TextAreaField, FileField, SelectField, IntegerField, FormField, Form, FieldList
-from wtforms.validators import DataRequired, InputRequired, NumberRange, Optional, Email
+import forms
+from forms import InsertRecipeForm, ContactForm, SendRecipeForm, meal_type_list, measureList
 
 # import variables setted in environment file if exist
 if os.path.exists("env.py"):
     import env
+
+
 
 # set upload folder and allowed extension for uploading recipe image
 UPLOAD_FOLDER = 'static/uploaded_img'
@@ -38,41 +39,6 @@ app.config['MAIL_PASSWORD']= os.environ.get('MAIL_PASSWORD')
 mail = Mail(app)
 mongo = PyMongo(app)
 db = mongo.db.recipes
-
-# list of measures used in add recipe form
-measureList=[('grams', 'grams'), ('decagrams', 'decagrams'), ('pieces', 'pieces'), ('pinch', 'pinch'), ('glasses', 'glasses'), ('liters', 'liters'), ('spoons', 'spoons'), ('tea spoons', 'tea spoons')]
-#  list of recipes categories, add it here in case of new category
-meal_type_list=[('warm_meals', 'Warm meals'), ('cold_meals', 'Cold meals'), ('drinks', 'Drinks'), ('desserts', 'Desserts')]
-
-
-class Ingredients(Form):
-    name = StringField('Ingredient name', validators=[InputRequired()])
-    amount = DecimalField('Amount', validators=[InputRequired()])
-    measure = SelectField('Measure', choices=measureList, validators=[InputRequired()])
-
-class Preparations(Form):
-    step = TextAreaField('Preparation step', validators=[InputRequired()])
-
-class InsertRecipeForm(FlaskForm):
-    recipe_name = StringField('Recipe name', validators=[InputRequired()])
-    meal_type = SelectField('Meal type', choices=meal_type_list, validators=[DataRequired()])
-    preparation_time = IntegerField('Preparation time (minutes)', validators=[DataRequired(), NumberRange(min=5, max=180)])
-    portions = IntegerField('Amount of portions', validators=[DataRequired(), NumberRange(min=1, max=6)])
-    meal_description = TextAreaField('Meal description', validators=[Optional()])
-    ingredients = FieldList(FormField(Ingredients), min_entries=3)
-    preparation = FieldList(FormField(Preparations), min_entries=1)
-    meal_image = FileField('Meal picture')
-
-class ContactForm(FlaskForm):
-    name = StringField('Name', validators=[InputRequired()])
-    email = StringField('email', validators=[DataRequired(), Email()])
-    subject = StringField('Subject', validators=[InputRequired()])
-    message = TextAreaField('Message', validators=[InputRequired()])
-    submit = SubmitField('Send')
-
-class sendRecipeForm(FlaskForm):
-    emailto = StringField('email address', validators=[DataRequired(), Email()])
-    submit = SubmitField('Send recipe on email')
 
 # home route, display all recipes
 @app.route('/')
@@ -164,8 +130,8 @@ def single_recipe(recipe_name):
     contactForm = ContactForm()
     recipe=mongo.db.recipe_base.find_one({'recipe_name': recipe_name})
     ingredients = {k:v for k,v in recipe.items() if "ingredient" in k}
-    meilRecipe = sendRecipeForm()
-    return render_template('singl_recipe.html', recipe=recipe, ingredients=ingredients, contactForm=contactForm, meilRecipe=meilRecipe)
+    mailRecipe = SendRecipeForm()
+    return render_template('singl_recipe.html', recipe=recipe, ingredients=ingredients, contactForm=contactForm, mailRecipe=mailRecipe)
 
 # route for sending contact message
 @app.route('/contact', methods=['GET','POST'])
@@ -175,21 +141,21 @@ def contact():
         email = request.form['email']
         subject = request.form['subject']
         message = request.form['message']
-        meilMsg=Message(subject=subject, sender=email, recipients=['deosiecki@gmail.com'])
-        meilMsg.body = 'You receive messaege: ' + message + 'from ' + name + 'who use email address ' + email
-        mail.send(meilMsg)
+        mailMsg=Message(subject=subject, sender=email, recipients=['deosiecki@gmail.com'])
+        mailMsg.body = 'You receive messaege: ' + message + 'from ' + name + 'who use email address ' + email
+        mail.send(mailMsg)
         flash('Message send succesfully', 'success')
         return redirect(url_for('home'))
 
 # route for sending recipe by email
 @app.route('/send-recipe-to-email/<recipe_name>', methods=['GET', 'POST'])
-def send_meil_recipe(recipe_name):
+def send_mail_recipe(recipe_name):
     recipe=mongo.db.recipe_base.find_one({'recipe_name': recipe_name})
     ingredients = {k:v for k,v in recipe.items() if "ingredient" in k}
     subject = "Your recipe for"
     emailto = request.form['emailto']
     recipeMsg = Message(subject=subject, recipients=[emailto])
-    recipeMsg.html = render_template('meil_recipe.html', recipe=recipe, ingredients=ingredients)
+    recipeMsg.html = render_template('mail_recipe.html', recipe=recipe, ingredients=ingredients)
     mail.send(recipeMsg)
     flash('Recipe succesfully send on your email address.', 'success')
     return redirect(url_for('home'))
---
+++ forms.py
@@ -0,0 +1,38 @@
+# form classes for recipe book app
+from flask_wtf import FlaskForm
+from wtforms import StringField, DecimalField, SubmitField, TextAreaField, FileField, SelectField, IntegerField, FormField, Form, FieldList
+from wtforms.validators import DataRequired, InputRequired, NumberRange, Optional, Email
+
+# list of measures used in add recipe form
+measureList=[('grams', 'grams'), ('decagrams', 'decagrams'), ('pieces', 'pieces'), ('pinch', 'pinch'), ('glasses', 'glasses'), ('liters', 'liters'), ('spoons', 'spoons'), ('tea spoons', 'tea spoons')]
+#  list of recipes categories, add it here in case of new category
+meal_type_list=[('warm_meals', 'Warm meals'), ('cold_meals', 'Cold meals'), ('drinks', 'Drinks'), ('desserts', 'Desserts')]
+
+class Ingredients(Form):
+    name = StringField('Ingredient name', validators=[InputRequired()])
+    amount = DecimalField('Amount', validators=[InputRequired()])
+    measure = SelectField('Measure', choices=measureList, validators=[InputRequired()])
+
+class Preparations(Form):
+    step = TextAreaField('Preparation step', validators=[InputRequired()])
+
+class InsertRecipeForm(FlaskForm):
+    recipe_name = StringField('Recipe name', validators=[InputRequired()])
+    meal_type = SelectField('Meal type', choices=meal_type_list, validators=[DataRequired()])
+    preparation_time = IntegerField('Preparation time (minutes)', validators=[DataRequired(), NumberRange(min=5, max=180)])
+    portions = IntegerField('Amount of portions', validators=[DataRequired(), NumberRange(min=1, max=6)])
+    meal_description = TextAreaField('Meal description', validators=[Optional()])
+    ingredients = FieldList(FormField(Ingredients), min_entries=3)
+    preparation = FieldList(FormField(Preparations), min_entries=1)
+    meal_image = FileField('Meal picture')
+
+class ContactForm(FlaskForm):
+    name = StringField('Name', validators=[InputRequired()])
+    email = StringField('email', validators=[DataRequired(), Email()])
+    subject = StringField('Subject', validators=[InputRequired()])
+    message = TextAreaField('Message', validators=[InputRequired()])
+    submit = SubmitField('Send')
+
+class SendRecipeForm(FlaskForm):
+    emailto = StringField('email address', validators=[DataRequired(), Email()])
+    submit = SubmitField('Send recipe on email')
