a190cc73ec0ba8cd381c04559dee711ea7f86461
dejvoss <deosiecki@gmail.com>
2020-10-08 15:53:07 +0200

    Change debug value to Fals

--- app.py
+++ app.py
@@ -275,5 +275,5 @@ def handle_bad_request(e):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=os.environ.get("PORT"),
-            debug=True)
+            debug=False)
 
